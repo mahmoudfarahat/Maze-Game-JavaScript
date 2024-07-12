90bdef0d89e4300d3fafc7cb780adf456d594c2f
Mahmoud Farahat <mahmoud.ped@gmail.com>
2024-07-12 21:10:56 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 
-const { Engine, Render, Runner, World, Bodies } = Matter
+const { Engine, Render, Runner, World, Bodies , Body  , Events } = Matter
 
 const cells = 3;
 const width = 600;
 const height = 600;
 
+const unitLength = width / cells
+
 const engine = Engine.create()
+engine.world.gravity.y = 0
 const { world } = engine
 
 const render = Render.create({
@@ -25,10 +28,10 @@ Runner.run(Runner.create(), engine)
 
 //walls
 const walls = [
-    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-    Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
+    Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+    Bodies.rectangle(0, height / 2,2, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 2, height, { isStatic: true }),
 
 ]
 
@@ -91,18 +94,126 @@ for (let neighbor of neighbors){
         continue;
     }
     // Remove a wall from either horizontals or verticals
+    if(direction === 'left')
+    {
+        verticals[row][column-1] = true
+    }else if (direction === 'right'){
+        verticals[row][column] = true
+    }else if (direction === 'up'){
+        horizontals[row-1][column] = true
+    }else if (direction === 'down'){
+        horizontals[row][column] = true
 
+    }
+// visit that next cell
 
-
+    stepThroughCell(nextRow,nextColumn)
 
 }
-    // visit that next cell
-
+    
 }
 
-stepThroughCell(startRow,startColumn)
+stepThroughCell(1,1)
 
 console.log(grid)
 
+horizontals.forEach((row ,rowIndex)=>{
+    row.forEach((open , columnIndex)=>{
+        if(open){
+            return
+        }
+        const wall = Bodies.rectangle(
+                columnIndex*unitLength + unitLength / 2 ,
+                rowIndex*unitLength + unitLength,
+                unitLength,
+                5 ,{
+                    label:'wall',
+                    isStatic : true
+                }
+        );  
+        World.add(world,wall)
+    })
+})   
+verticals.forEach((row,rowIndex) => {
+    row.forEach((open,columnIndex) => {
+        if(open){
+            return
+        }
 
- 
+        const wall  = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            5,
+            unitLength,
+            {
+                label:'wall',
+                isStatic:true
+            }
+        )
+
+   World.add(world,wall)
+    })
+})
+
+// Goal
+const goal = Bodies.rectangle(
+    width- unitLength /2 ,
+    height - unitLength / 2,
+    unitLength *.7,
+    unitLength *.7,{
+        label:'goal',
+        isStatic:true
+    }
+);
+
+
+World.add(world,goal)
+
+// Ball
+
+const ball = Bodies.circle(
+    unitLength/ 2,
+    unitLength /2 ,
+    unitLength / 4 , {
+        label: 'ball'
+    }
+)
+World.add(world,ball)
+
+document.addEventListener('keydown',event =>{
+    const {x , y } = ball.velocity
+
+        if  (event.keyCode === 87){ //w
+                Body.setVelocity(ball, {x, y:y-5})
+        }
+        if  (event.keyCode === 68){ //d
+            Body.setVelocity(ball, {x:x +5, y})
+
+        }
+        if  (event.keyCode === 83){ //s
+            Body.setVelocity(ball, {x, y:y+5})
+
+        }
+        if  (event.keyCode === 65){//a
+            Body.setVelocity(ball, {x:x-5, y})
+
+        }
+})
+
+
+// Win Condition
+
+Events.on(engine,'collisionStart', event=>{
+   event.pairs.forEach(collision =>{
+     const labels = ['ball','goal'];
+     if(labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label))
+     {
+           world.gravity.y = 1;
+           world.bodies.forEach(body =>{
+            if(body.label === 'wall'){
+                Body.setStatic(body,false)
+            }
+           })
+     }
+   })
+})
