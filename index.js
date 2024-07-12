f50c9703e82da7cbb2fa293dc374dada75fa8ebf
Mahmoud Farahat <mahmoud.ped@gmail.com>
2024-07-12 14:28:17 +0300

    first commit

--- index.js
+++ index.js
@@ -34,7 +34,19 @@ const walls = [
 
 World.add(world, walls)
 
+const shuffle = (arr) => {
+let counter = arr.length;
+while(counter > 0){
+    const index = Math.floor(Math.random() * counter)
 
+    counter--;
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp
+
+}
+return arr
+}
 const grid = Array(cells) //more rows
     .fill(null)
     .map(() => Array(cells).fill(false)) // more columns
@@ -53,21 +65,44 @@ const startColumn = Math.floor(Math.random() * cells);
 
 const stepThroughCell = (row, column) =>{
     // if i have visited the cell at [row,column] , then return
-
+    if(grid[row][column]){
+        return
+    }
     // Mark this cell as being visited
+    grid[row][column] = true
 
     // Assemble randomly-ordered list of neighbors
+const neighbors = shuffle([ 
+        [row-1 , column, 'up'], // above
+        [row, column+1 ,'right'],  // Right
+        [row+1 , column ,'down'], // Below
+        [row , column-1, 'left']  // left 
 
+]);
     // for each neighbor
-
+for (let neighbor of neighbors){
+    const [nextRow , nextColumn , direction] = neighbor
     // see if that neihbor is out of bounds
-
+    if(nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells){
+        continue; // dont leave the loop but move next 
+    }
     // if we have visited that neighbor, continue to next neighbor
-
+    if(grid[nextRow][nextColumn]){
+        continue;
+    }
     // Remove a wall from either horizontals or verticals
 
+
+
+
+}
     // visit that next cell
 
 }
 
 stepThroughCell(startRow,startColumn)
+
+console.log(grid)
+
+
+ 
